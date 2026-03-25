14cced7f7bbc4b8d70e6fb5a01a1bf5a69f8cc23
Jake Wood <hello@jakewood.org>
2026-03-25 17:02:12 -0500

    Allow http, maybe? (#31)

--- frontend/src/lib/auth-api.ts
+++ frontend/src/lib/auth-api.ts
@@ -2,7 +2,11 @@ import { AuthCredentials } from "../types/auth";
 
 export const authApi = {
   async login(credentials: AuthCredentials) {
-    const response = await fetch(`//${credentials.ipAddress}/auth/login`, {
+    let url = `//${credentials.ipAddress}/auth/login`;
+    if (credentials.ipAddress.startsWith("http")) {
+      url = `${credentials.ipAddress}/auth/login`
+    }
+    const response = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ password: credentials.password }),
